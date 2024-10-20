6b6818f606c4235331517541cc13501cf78ecdcf
sumittt3 <sumitthakur16000@gmail.com>
2024-10-20 21:23:37 +0530

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,81 +1,24 @@
-import {Link, NavLink} from "react-router-dom";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="shadow-lg sticky z-50 top-0 bg-white">
-      <nav className="border-gray-200 px-4 lg:px-6 py-4 transition duration-300 ease-in-out">
+      <nav className="border-gray-200 px-4 lg:px-6 py-6 transition duration-300 ease-in-out">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <span className="self-center text-3xl font-bold text-gray-800 transition duration-300 transform hover:scale-105">
               Sumit Thakur
             </span>
           </Link>
-          <div className="hidden lg:flex lg:order-2">
-            <ul className="flex space-x-6">
-              <li>
-                <NavLink
-                  to=""
-                  className={({isActive}) =>
-                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
-                      isActive ? "border-b-2 border-orange-600" : ""
-                    }`
-                  }
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/about"
-                  className={({isActive}) =>
-                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
-                      isActive ? "border-b-2 border-orange-600" : ""
-                    }`
-                  }
-                >
-                  About
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/projects"
-                  className={({isActive}) =>
-                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
-                      isActive ? "border-b-2 border-orange-600" : ""
-                    }`
-                  }
-                >
-                  Projects
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/skills"
-                  className={({isActive}) =>
-                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
-                      isActive ? "border-b-2 border-orange-600" : ""
-                    }`
-                  }
-                >
-                  Skills
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/resume"
-                  className={({isActive}) =>
-                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
-                      isActive ? "border-b-2 border-orange-600" : ""
-                    }`
-                  }
-                >
-                  Resume
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+          {/* Mobile Menu Button */}
           <div className="lg:hidden flex items-center">
-            <button className="text-gray-800 hover:text-orange-600 focus:outline-none transition duration-300 ease-in-out transform hover:scale-110">
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="text-gray-800 hover:text-orange-600 focus:outline-none transition duration-300 ease-in-out transform hover:scale-110"
+            >
               <svg
                 className="w-6 h-6"
                 fill="none"
@@ -92,8 +35,83 @@ export default function Header() {
               </svg>
             </button>
           </div>
+          {/* Desktop Menu */}
+          <div
+            className={`${
+              isMenuOpen ? "block" : "hidden"
+            } lg:flex lg:items-center lg:space-x-6 lg:w-auto w-full mt-4 lg:mt-0`}
+          >
+            <ul className="flex flex-col lg:flex-row items-center lg:space-x-6 space-y-4 lg:space-y-0">
+              <li>
+                <NavLink
+                  to=""
+                  className={({ isActive }) =>
+                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
+                      isActive ? "border-b-2 border-orange-600" : ""
+                    }`
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
+                      isActive ? "border-b-2 border-orange-600" : ""
+                    }`
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/projects"
+                  className={({ isActive }) =>
+                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
+                      isActive ? "border-b-2 border-orange-600" : ""
+                    }`
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Projects
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/skills"
+                  className={({ isActive }) =>
+                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
+                      isActive ? "border-b-2 border-orange-600" : ""
+                    }`
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Skills
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/resume"
+                  className={({ isActive }) =>
+                    `text-gray-800 hover:text-orange-600 font-medium py-2 px-4 transition duration-300 ease-in-out ${
+                      isActive ? "border-b-2 border-orange-600" : ""
+                    }`
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Resume
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </header>
   );
 }
+
