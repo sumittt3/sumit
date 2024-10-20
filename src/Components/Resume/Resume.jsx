98aeb6e2b4b5286e4d067a746bc43719669c401b
SumitThakur1600 <sumitthakur1600@gmail.com>
2024-10-20 21:14:51 +0530

    First Commit

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Resume() {
-  const resumeUrl = "/dsf.pdf"; // Update the path if necessary
+  const resumeUrl = "./dsf.pdf"; // Update the path if necessary
 
   const handleDownload = () => {
     const link = document.createElement("a");
